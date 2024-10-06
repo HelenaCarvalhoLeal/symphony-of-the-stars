4e1cf8309277746c89657116afcad72bfe3027e8
rafaseto <rafael08.0807@gmail.com>
2024-10-06 00:26:57 -0300

    style home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,14 @@ import './ImageList.css'
 function Home() {
     return (
         <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>Bem-vindo à Aplicação de Imagens</h1>
+            <h1 style={{ fontSize: '52px' }}>Close Your Eyes And Listen</h1>
+            <h3 style={{ fontSize: '18px' }}>A new way of exploring the universe</h3>
             <div style={{ marginTop: '20px' }}>
                 <Link to="/upload">
-                    <button style={{ margin: '10px', padding: '10px 20px' }}>Selecionar Imagem do Computador</button>
+                    <button style={{ margin: '10px', padding: '130px 150px' }}>Build Your Own Symphony</button>
                 </Link>
                 <Link to="/predefined">
-                    <button style={{ margin: '10px', padding: '10px 20px' }}>Escolher Imagem Pré-definida</button>
+                    <button style={{ margin: '10px', padding: '130px 150px' }}>Escolher Imagem Pré-definida</button>
                 </Link>
             </div>
         </div>
